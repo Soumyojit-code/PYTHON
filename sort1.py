1e31b128b9c2bcf66b39a5b0bdb7238da5bbb81b
Soumyojit Banerjee <144514288+Soumyojit-code@users.noreply.github.com>
2025-11-19 21:28:57 +0530

    Add files via upload

---
+++ sort1.py
@@ -0,0 +1,9 @@
+""""". An online store records the prices of items in an array.
+Write a program to sort the prices from lowest to highest.
+Display the sorted output."""
+
+Prices={120,57,200,150,87,45}
+Sort=sorted(Prices)
+print("Sorted prices (lowest to highest):")
+for Prices in Sort:
+ print(Prices)
